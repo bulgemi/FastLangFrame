82a8848b1727e56259c009603d0a57c42bbf4463
kim donghun <bulgemi77@gmail.com>
2026-03-24 10:43:12 +0900

    refactor: standardize Docker builds to use a common root context and explicitly copy project and framework files for improved structure.

--- templates/simple_agent/<%project_name%>/docker/docker-build.sh
+++ templates/simple_agent/<%project_name%>/docker/docker-build.sh
@@ -2,4 +2,4 @@
 PROJECT_NAME="<%project_name%>"
 IMAGE_NAME="${PROJECT_NAME}:latest"
 echo "Building Docker image: ${IMAGE_NAME}..."
-docker build -t ${IMAGE_NAME} -f docker/Dockerfile .
+docker build -t ${IMAGE_NAME} -f docker/Dockerfile ../../..
